572c96c893cd43c5be0950b0cfcc29e8eeba386c
DCliuzhe <liuzhe61@sjtu.edu.cn>
2026-04-04 19:06:01 +0800

    Edit Tcl Scripts

--- tcl/synthesis/run.tcl
+++ tcl/synthesis/run.tcl
@@ -1,8 +1,8 @@
 #-------------------Specify Libraries------------------
-set target_library "/PATH/skywater-pdk/db/sky130_fd_sc_hd__tt_025C_1v80.db"
-set link_library "* /PATH/skywater-pdk/db/sky130_fd_sc_hd__tt_025C_1v80.db"
+set target_library "/PATH/TO/YOUR/PDK"
+set link_library "* /PATH/TO/YOUR/PDK"
 
-source ../scripts/ara.tcl
+source /PATH/TO/ara.tcl
 
 set_host_options -max_cores 3
 
@@ -11,18 +11,18 @@ uniquify
 current_design ara_00000004_00001000_00000001_38_1_1_1_00000080_00000040_793242
 link
 set_flatten false
-source ../scripts/constraint.sdc
+source /PATH/TO/constraint.sdc
 compile
 
 ##----------------------Write Outputs--------------------
-write -f verilog -output /PATH/work/ARA/output/ara_netlist.v
-write -format ddc -output /PATH/work/ARA/output/ara.ddc
+write -f verilog -output /PATH/TO/ara_netlist.v
+write -format ddc -output /PATH/TO/ara.ddc
 
 ##----------------------Write Reports--------------------
-report_timing -significant_digits 4 -max_path 5 > /PATH/work/ARA/report/ARA_timing.rpt
-report_area -hierarchy > /PATH/work/ARA/report/ARA_area.rpt
-report_power -hierarchy > /PATH/work/ARA/report/ARA_power.rpt
-report_qor > /PATH/work/ARA/report/ARA_qor.rpt
-report_design > /PATH/work/ARA/report/ARA_design.rpt
+report_timing -significant_digits 4 -max_path 5 > /PATH/TO/ARA_timing.rpt
+report_area -hierarchy > /PATH/TO/ARA_area.rpt
+report_power -hierarchy > /PATH/TO/ARA_power.rpt
+report_qor > /PATH/TO/ARA_qor.rpt
+report_design > /PATH/TO/ARA_design.rpt
 
 exit
